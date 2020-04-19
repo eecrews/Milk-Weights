7e929917002aa2ad2d5e269cffcddcd4cad7ea02
eecrews <61025723+eecrews@users.noreply.github.com>
2020-04-19 18:35:51 -0500

    Add imports to GUI.java

--- GUI.java
+++ GUI.java
@@ -1,9 +1,16 @@
 package application;
 
-
-// test
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * GUI - TODO Describe purpose of this user-defined type
