c857df3e43eb632da210d12338ae15983bc55201
eecrews <61025723+eecrews@users.noreply.github.com>
2020-04-19 18:34:36 -0500

    Add static final instance fields to GUI.java
    Includes id, year, month, date, and weight entry areas. Also includes tentative application window width and height.

--- GUI.java
+++ GUI.java
@@ -10,6 +10,16 @@ import javafx.stage.Stage;
  * 
  */
 public class GUI extends Application {
+	private static final int WINDOW_WIDTH = 700;
+	private static final int WINDOW_HEIGHT = 500;
+	private static final String APP_TITLE = "Milk Weights";
+	
+	private static final HBox idEntry = new HBox(new Label(" ID "), new TextField());
+	private static final HBox yearEntry = new HBox(new Label(" Year "), new TextField());
+	private static final HBox monthEntry = new HBox(new Label(" Month "), new TextField());
+	private static final HBox dateEntry = new HBox(new Label(" Date "), new TextField());
+	private static final HBox weightEntry = new HBox(new Label(" Weight "), new TextField());
+	
 
 	@Override
 	public void start(Stage arg0) throws Exception {
