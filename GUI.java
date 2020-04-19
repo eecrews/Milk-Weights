375336c21d0dffc829c90cd98d054c4bcd39b1b6
eecrews <61025723+eecrews@users.noreply.github.com>
2020-04-19 16:20:45 -0500

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,19 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * GUI - TODO Describe purpose of this user-defined type
+ * @author crews (2020)
+ * 
+ */
+public class GUI extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
