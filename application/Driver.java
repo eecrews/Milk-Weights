352077c7e08c7e10d475dfe9b0fb7ce2ef814f6c
eecrews <eecrews@wisc.edu>
2020-04-25 18:00:31 -0500

    Add parseFile method

--- application/Driver.java
+++ application/Driver.java
@@ -1,18 +1,49 @@
 package application;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 
 /**
  * Driver - TODO Describe purpose of this user-defined type
  * 
  */
-public class Driver extends Application {
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-
+public class Driver {
+	
+	private static ArrayList<Farm> farmArray = new ArrayList<Farm>();
+	
+	public static void parseFile(String fileName) {
+		ArrayList<LocalDate> dateArray = new ArrayList<LocalDate>();
+		ArrayList<String> farmIDArray = new ArrayList<String>();
+		ArrayList<Integer> weightArray = new ArrayList<Integer>();
+		
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line = "";
+			String splitBy = ",";
+			while((line = br.readLine()) != null) {
+				String[] lineInfo = line.split(splitBy);
+				dateArray.add(LocalDate.parse(lineInfo[0]));
+				farmIDArray.add(lineInfo[1]);
+				weightArray.add(Integer.parseInt(lineInfo[2]));
+				
+			}
+		}
+		
+		for(int i=0; i<farmIDArray.size(); i++) {
+			if(!farmArray.contains(farmIDArray.get(i))) {
+				Farm newFarm = new Farm(farmIDArray.get(i));
+				newFarm.addEntry(weightArray.get(i), dateArray.get(i));
+				farmArray.add(newFarm);
+				
+			}
+		}
+		
 	}
+
+	
 
 }
