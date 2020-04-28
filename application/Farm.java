415190128bd787f85057d31c7314a94012280d47
TusharLaksh <tusharlaksh@gmail.com>
2020-04-28 17:59:44 -0500

    ID from int to String in farm class

--- application/Farm.java
+++ application/Farm.java
@@ -1,35 +1,38 @@
+package application;
+
 import java.time.LocalDate;
 
 public class Farm {
 	String farmID;
 	mArray entryList;
-	
+
 	public Farm(String farmID) {
 		this.farmID = farmID;
 		entryList = new mArray();
 	}
-	
+
 	public void addEntry(int milkWeight, LocalDate date) {
-		entryList.addDay(milkWeight, date, farmID);
+		entryList.addDay((double) milkWeight, date, farmID);
 	}
-	
+
 	public String getID() {
 		return farmID;
 	}
-	
+
 	public mArray getEntryList() {
 		return entryList;
 	}
+
 	public int getMilkWeight(LocalDate date) {
-		Node[] entries= entryList.year;	
+		Node[] entries = entryList.year;
 		Node entryFound = null;
-		for(Node entry: entries) {
-			if(entry.getMilkDate().equals(date))
+		for (Node entry : entries) {
+			if (entry.getMilkDate().equals(date))
 				entryFound = entry;
 		}
-		if(entryFound == null)
+		if (entryFound == null)
 			return -1;
-		return entryFound.milkWeight;
+		return (int) entryFound.milkWeight;
 	}
-	
+
 }
--- application/mArray.java
+++ application/mArray.java
@@ -3,79 +3,63 @@ package application;
 import java.time.LocalDate;
 
 public class mArray {
-  Node[] year;
+	Node[] year;
 
-  public mArray() {
-    year = new Node[367];
+	public mArray() {
+		year = new Node[367];
 
+	}
 
-  }
-
-  public void addDay(double milk, LocalDate date, int ID) {
-    int index = date.getDayOfYear();
-    year[index] = new Node(milk, date, ID);
-  }
-  
-  
+	public void addDay(double milk, LocalDate date, String ID) {
+		int index = date.getDayOfYear();
+		year[index] = new Node(milk, date, ID);
+	}
 
 }
-
-
 
 class Node {
-  double milkWeight;
-  LocalDate milkDate;
-  int farmID;
-  Node next;
+	double milkWeight;
+	LocalDate milkDate;
+	String farmID;
+	Node next;
 
+	public double getMilkWeight() {
+		return milkWeight;
+	}
 
-  public double getMilkWeight() {
-    return milkWeight;
-  }
+	public void setMilkWeight(double milkWeight) {
+		this.milkWeight = milkWeight;
+	}
 
+	public LocalDate getMilkDate() {
+		return milkDate;
+	}
 
-  public void setMilkWeight(double milkWeight) {
-    this.milkWeight = milkWeight;
-  }
+	public void setMilkDate(LocalDate milkDate) {
+		this.milkDate = milkDate;
+	}
 
+	public String getFarmID() {
+		return farmID;
+	}
 
-  public LocalDate getMilkDate() {
-    return milkDate;
-  }
+	public void setFarmID(String farmID) {
+		this.farmID = farmID;
+	}
 
+	public Node getNext() {
+		return next;
+	}
 
-  public void setMilkDate(LocalDate milkDate) {
-    this.milkDate = milkDate;
-  }
+	public void setNext(Node next) {
+		this.next = next;
+	}
 
-
-  public int getFarmID() {
-    return farmID;
-  }
-
-
-  public void setFarmID(int farmID) {
-    this.farmID = farmID;
-  }
-
-
-  public Node getNext() {
-    return next;
-  }
-
-
-  public void setNext(Node next) {
-    this.next = next;
-  }
-
-
-  public Node(double milkWeight, LocalDate milkDate, int farmID) {
-    this.milkWeight = milkWeight;
-    this.milkDate = milkDate;
-    this.farmID = farmID;
-    next = null;
-  }
-
+	public Node(double milkWeight, LocalDate milkDate, String farmID) {
+		this.milkWeight = milkWeight;
+		this.milkDate = milkDate;
+		this.farmID = farmID;
+		next = null;
+	}
 
 }
-
