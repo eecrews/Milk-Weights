7eb6d9d40140188c9db852969e02f8e41ff85344
eecrews <eecrews@wisc.edu>
2020-04-30 19:23:35 -0500

    remove daydoesnotexistexception as it was causing issues

--- application/Driver.java
+++ application/Driver.java
@@ -18,24 +18,24 @@ import javafx.application.Application;
  * 
  */
 public class Driver {
-	
+
 	public static class InformationOmittedException extends Exception {
-		
+
 		public InformationOmittedException() {
 			super();
 		}
-		
+
 		public InformationOmittedException(String errorMessage) {
 			super(errorMessage);
 		}
 	}
-	
+
 	public static class IncorrectFileTypeException extends Exception {
-		
+
 		public IncorrectFileTypeException() {
 			super();
 		}
-		
+
 		public IncorrectFileTypeException(String errorMessage) {
 			super(errorMessage);
 		}
@@ -45,34 +45,28 @@ public class Driver {
 		public FarmDoesNotExistException() {
 			super();
 		}
+
 		public FarmDoesNotExistException(String errorMessage) {
 			super(errorMessage);
 		}
 	}
-	
+
 	public static class YearDoesNotExistException extends Exception {
 		public YearDoesNotExistException() {
 			super();
 		}
+
 		public YearDoesNotExistException(String errorMessage) {
 			super(errorMessage);
 		}
 	}
-	
+
 	public static class MonthDoesNotExistException extends Exception {
 		public MonthDoesNotExistException() {
 			super();
 		}
+
 		public MonthDoesNotExistException(String errorMessage) {
-			super(errorMessage);
-		}
-	}
-	
-	public static class DayDoesNotExistException extends Exception {
-		public DayDoesNotExistException() {
-			super();
-		}
-		public DayDoesNotExistException(String errorMessage) {
 			super(errorMessage);
 		}
 	}
@@ -82,152 +76,158 @@ public class Driver {
 	private static ArrayList<String> farmIDs = new ArrayList<String>();
 	private static ArrayList<Integer> years = new ArrayList<Integer>();
 	private static ArrayList<Integer> months = new ArrayList<Integer>();
-	private static ArrayList<Integer> days = new ArrayList<Integer>();
 
-	public static void parseFile(String fileName) throws FileNotFoundException, IOException, InformationOmittedException, IncorrectFileTypeException {		
-		if(!fileName.contains(".csv"))
+	public static void parseFile(String fileName)
+			throws FileNotFoundException, IOException,
+			InformationOmittedException, IncorrectFileTypeException {
+		if (!fileName.contains(".csv"))
 			throw new IncorrectFileTypeException();
 		ArrayList<LocalDate> dateArray = new ArrayList<LocalDate>();
 		ArrayList<String> farmIDArray = new ArrayList<String>();
 		ArrayList<Double> weightArray = new ArrayList<Double>();
-	
+
 		DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-	            .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
-	            .appendOptional(DateTimeFormatter.ofPattern(("yyyy-MM-d")))
-	            .appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-	            .appendOptional(DateTimeFormatter.ofPattern("yyyy-M-dd"))
-	            .toFormatter();
-		
+				.appendOptional(DateTimeFormatter.ofPattern("yyyy-M-d"))
+				.appendOptional(DateTimeFormatter.ofPattern(("yyyy-MM-d")))
+				.appendOptional(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+				.appendOptional(DateTimeFormatter.ofPattern("yyyy-M-dd"))
+				.toFormatter();
+
 		boolean informationOmitted = false;
-		
+
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			String line = "";
 			String splitBy = ",";
 			br.readLine();
 			while ((line = br.readLine()) != null) {
-					String[] lineInfo = line.split(splitBy);
-					if(lineInfo[0] == null || lineInfo[1] == null || lineInfo[2] == null) {
-						continue;
-					}
-					if(lineInfo[1].length() < 4 || !lineInfo[1].substring(0,4).contentEquals("Farm")) {
-						informationOmitted = true;
-						continue;
-					}
-					
-					LocalDate dateToBeAdded;
-					String farmIDToBeAdded;
-					Double weightToBeAdded;
-										
-					try {
-						dateToBeAdded = LocalDate.parse(lineInfo[0],formatter);
-						farmIDToBeAdded = lineInfo[1];
-						weightToBeAdded = Double.parseDouble(lineInfo[2]);
-					} catch(DateTimeParseException e) {
-						informationOmitted = true;
-						continue;
-					} catch(NumberFormatException e) {
-						informationOmitted = true;
-						continue;
-					}
-						dateArray.add(dateToBeAdded);
-						farmIDArray.add(farmIDToBeAdded);
-						weightArray.add(weightToBeAdded);
-		} 
-
-
-		for (int i = 0; i < farmIDArray.size(); i++) {
-			if (farmIDs.isEmpty() || !farmIDs.contains(farmIDArray.get(i))) { 
-				
-				farmIDs.add(farmIDArray.get(i));
-				Farm newFarm = new Farm(farmIDArray.get(i));
-				newFarm.addEntry(weightArray.get(i), dateArray.get(i));
-				farmArray.add(newFarm);
-				
-				if(!years.contains(dateArray.get(i).getYear()))
-					years.add(dateArray.get(i).getYear());
-				
-				if(!months.contains(dateArray.get(i).getMonthValue())) 
-					months.add(dateArray.get(i).getMonthValue());
-				
-				if(!days.contains(dateArray.get(i).getDayOfYear()))
-					days.add(dateArray.get(i).getDayOfYear());
-				
-			} else {
-				int indexOfRepeat;
-				for(indexOfRepeat=0; indexOfRepeat<farmArray.size(); indexOfRepeat++) {
-					if(farmArray.get(indexOfRepeat).getID().contentEquals(farmIDArray.get(i)))
-						break;
+				String[] lineInfo = line.split(splitBy);
+				if (lineInfo[0] == null || lineInfo[1] == null
+						|| lineInfo[2] == null) {
+					continue;
 				}
-				farmArray.get(indexOfRepeat).addEntry(weightArray.get(i), dateArray.get(i));
+				if (lineInfo[1].length() < 4
+						|| !lineInfo[1].substring(0, 4).contentEquals("Farm")) {
+					informationOmitted = true;
+					continue;
+				}
+
+				LocalDate dateToBeAdded;
+				String farmIDToBeAdded;
+				Double weightToBeAdded;
+
+				try {
+					dateToBeAdded = LocalDate.parse(lineInfo[0], formatter);
+					farmIDToBeAdded = lineInfo[1];
+					weightToBeAdded = Double.parseDouble(lineInfo[2]);
+				} catch (DateTimeParseException e) {
+					informationOmitted = true;
+					continue;
+				} catch (NumberFormatException e) {
+					informationOmitted = true;
+					continue;
+				}
+				dateArray.add(dateToBeAdded);
+				farmIDArray.add(farmIDToBeAdded);
+				weightArray.add(weightToBeAdded);
 			}
+
+			for (int i = 0; i < farmIDArray.size(); i++) {
+				if (farmIDs.isEmpty()
+						|| !farmIDs.contains(farmIDArray.get(i))) {
+
+					farmIDs.add(farmIDArray.get(i));
+					Farm newFarm = new Farm(farmIDArray.get(i));
+					newFarm.addEntry(weightArray.get(i), dateArray.get(i));
+					farmArray.add(newFarm);
+
+					if (!years.contains(dateArray.get(i).getYear()))
+						years.add(dateArray.get(i).getYear());
+
+					if (!months.contains(dateArray.get(i).getMonthValue()))
+						months.add(dateArray.get(i).getMonthValue());
+
+				} else {
+					int indexOfRepeat;
+					for (indexOfRepeat = 0; indexOfRepeat < farmArray
+							.size(); indexOfRepeat++) {
+						if (farmArray.get(indexOfRepeat).getID()
+								.contentEquals(farmIDArray.get(i)))
+							break;
+					}
+					farmArray.get(indexOfRepeat).addEntry(weightArray.get(i),
+							dateArray.get(i));
+				}
+			}
+
+			operator = new MilkOperations(
+					farmArray.toArray(new Farm[farmArray.size()]));
+
 		}
 
-		operator = new MilkOperations(farmArray.toArray(new Farm[farmArray.size()]));
-		
-		}
-		
-		if(informationOmitted) 
+		if (informationOmitted)
 			throw new InformationOmittedException();
-		
+
 	}
-	
+
 	public static boolean doesFarmExist(String farm) {
 		return farmIDs.contains(farm);
 	}
-	
+
 	public static boolean doesYearExist(int year) {
 		return years.contains(year);
 	}
-	
+
 	public static boolean doesMonthExist(int month) {
 		return months.contains(month);
 	}
-	
-	public static boolean doesDayExist(int day) {
-		return days.contains(day);
-	}
-	
+
 	public static String printFarms() {
 		String output = "";
-		for(int i=0; i<farmArray.size(); i++) {
-			output+= farmArray.get(i).getID() + "\n";
+		for (int i = 0; i < farmArray.size(); i++) {
+			output += farmArray.get(i).getID() + "\n";
 			output += farmArray.get(i).printAllEntries();
 			output += "\n\n";
 		}
-		
+
 		return output;
 	}
 
-
-	public static String printFarmReport(String farmID, int year) throws FarmDoesNotExistException, YearDoesNotExistException {
-		if(!doesFarmExist(farmID)) {
+	public static String printFarmReport(String farmID, int year)
+			throws FarmDoesNotExistException, YearDoesNotExistException {
+		if (!doesFarmExist(farmID)) {
 			throw new FarmDoesNotExistException();
 		}
-		if(!doesYearExist(year)) {
+		if (!doesYearExist(year)) {
 			throw new YearDoesNotExistException();
 		}
-		ArrayList<MilkOperations.MilkData> milkDataList = operator.farmReport(farmID, year);
+		ArrayList<MilkOperations.MilkData> milkDataList = operator
+				.farmReport(farmID, year);
 		String output = "";
 		for (int i = 0; i < milkDataList.size(); i++) {
-			output += "\nMonth: " + (i + 1) + "\tTotal Weight: " + milkDataList.get(i).getAmount()
-					+ "\tPercent of yearly weight: " + milkDataList.get(i).getPercentage();
+			output += "\nMonth: " + (i + 1) + "\tTotal Weight: "
+					+ milkDataList.get(i).getAmount()
+					+ "\tPercent of yearly weight: "
+					+ milkDataList.get(i).getPercentage();
 		}
 
 		return output;
 	}
 
-	public static String printMonthlyReport(int year, int month) throws MonthDoesNotExistException, YearDoesNotExistException {
-		if(!doesMonthExist(month))
+	public static String printMonthlyReport(int year, int month)
+			throws MonthDoesNotExistException, YearDoesNotExistException {
+		if (!doesMonthExist(month))
 			throw new MonthDoesNotExistException();
-		if(!doesYearExist(year))
+		if (!doesYearExist(year))
 			throw new YearDoesNotExistException();
 		DecimalFormat df = new DecimalFormat("#.####");
-		ArrayList<MilkOperations.MilkData> milkDataList = operator.MonthlyReport(year, month);
+		ArrayList<MilkOperations.MilkData> milkDataList = operator
+				.MonthlyReport(year, month);
 		String output = "";
 		for (int i = 0; i < milkDataList.size(); i++) {
-			output += "\n" + milkDataList.get(i).getF().getID() + ":\tTotal Weight: " + milkDataList
-					.get(i).getAmount() + "\tPercent of total for month: " + df.format(milkDataList.get(i)
-							.getPercentage());
+			output += "\n" + milkDataList.get(i).getF().getID()
+					+ ":\tTotal Weight: " + milkDataList.get(i).getAmount()
+					+ "\tPercent of total for month: "
+					+ df.format(milkDataList.get(i).getPercentage());
 		}
 
 		return output;
@@ -235,41 +235,41 @@ public class Driver {
 	}
 
 	public static String printDateRangeReport(int year1, int month1, int day1,
-			int month2, int day2) throws DayDoesNotExistException, MonthDoesNotExistException, YearDoesNotExistException {
-		if(!doesYearExist(year1)) 
+			int month2, int day2) throws MonthDoesNotExistException, YearDoesNotExistException {
+		if (!doesYearExist(year1))
 			throw new YearDoesNotExistException();
-		if(!doesDayExist(day1) || !doesDayExist(day2))
-			throw new DayDoesNotExistException();
-		if(!doesMonthExist(month1) || !doesMonthExist(month2))
+		if (!doesMonthExist(month1) || !doesMonthExist(month2))
 			throw new MonthDoesNotExistException();
-		
-		ArrayList<MilkOperations.MilkData> milkDataList = operator.DateRangeReport(year1, month1, day1, month2, day2);
+
+		ArrayList<MilkOperations.MilkData> milkDataList = operator
+				.DateRangeReport(year1, month1, day1, month2, day2);
 		String output = "";
 		for (int i = 0; i < milkDataList.size(); i++) {
-			output += "\n" + milkDataList.get(i).getF().getID() + ":\tTotal Weight: " + milkDataList
-					.get(i).getAmount() + "\tPercent of total over range: " + milkDataList.get(i)
-							.getPercentage();
+			output += "\n" + milkDataList.get(i).getF().getID()
+					+ ":\tTotal Weight: " + milkDataList.get(i).getAmount()
+					+ "\tPercent of total over range: "
+					+ milkDataList.get(i).getPercentage();
 		}
 
 		return output;
 	}
 
-
-	public static String printAnnualReport(int year){
+	public static String printAnnualReport(int year) {
 		DecimalFormat df = new DecimalFormat("#.####");
-		ArrayList<MilkOperations.MilkData> milkDataList = operator.AnnualReport(year);
+		ArrayList<MilkOperations.MilkData> milkDataList = operator
+				.AnnualReport(year);
 		String output = "";
-		for(int i=0; i<milkDataList.size(); i++) {
-			output += "\n" + milkDataList.get(i).getF().getID() + ":\tTotal Weight: " +
-					milkDataList.get(i).getAmount() + "\tPercent of total for year: " +
-					df.format(milkDataList.get(i)
-							.getPercentage());
+		for (int i = 0; i < milkDataList.size(); i++) {
+			output += "\n" + milkDataList.get(i).getF().getID()
+					+ ":\tTotal Weight: " + milkDataList.get(i).getAmount()
+					+ "\tPercent of total for year: "
+					+ df.format(milkDataList.get(i).getPercentage());
 		}
 
-		return output; 
+		return output;
 
 	}
-	
+
 	public static void main(String[] args) {
 		Application.launch(GUI.class, args);
 	}
